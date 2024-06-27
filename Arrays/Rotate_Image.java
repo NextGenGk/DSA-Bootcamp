0349dd7cfcfda8c515667ce6318e0acda0436362
GAURAV KUMAR <atkola12345@gmail.com>
2024-06-27 22:46:11 +0530

    Update Rotate_Image.java

--- Arrays/Rotate_Image.java
+++ Arrays/Rotate_Image.java
@@ -16,6 +16,7 @@ public class Rotate_Image {
             }
         }
 
+        // Print the Matrix
         for (int i=0; i<n; i++) {
             for (int j=0; j<n; j++) {
                 System.out.print(ansMatrix[i][j] + " ");
