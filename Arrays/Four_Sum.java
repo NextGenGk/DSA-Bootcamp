b93cbd7eda67b8fc1e006cf98d76aa278b21ec8f
NextGenGk <atkola12345@gmail.com>
2024-06-28 21:04:51 +0530

    Update Code Four_Sum

--- Arrays/Four_Sum.java
+++ Arrays/Four_Sum.java
@@ -44,29 +44,34 @@ public class Four_Sum {
     // Reason: we are using a set data structure and a list to store the quads. This results in the first term. And the
     // second space is taken by the set data structure we are using to store the array elements. At most, the set can contain approximately all the array elements and so the space complexity is O(N).
     public static List<List<Integer>> fourSum2(int[] nums, int target) {
-        // To return the answer
-        HashSet<List<Integer>> mainSet = new HashSet<>();
         int n = nums.length; // size of the array
+        Set<List<Integer>> st = new HashSet<>();
 
-        for (int i=0; i<n; i++) {
-            for (int j=i+1; j<n; j++) {
-                HashSet<Integer> hashSet = new HashSet<>();
-                for (int k=j+1; k<n; k++) {
-                    int fourth = target - (nums[i] + nums[j] + nums[k]);
-                    if (hashSet.contains(fourth)) {
+        // checking all possible quadruplets:
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                Set<Long> hashset = new HashSet<>();
+                for (int k = j + 1; k < n; k++) {
+                    // taking bigger data type
+                    // to avoid integer overflow:
+                    long sum = nums[i] + nums[j];
+                    sum += nums[k];
+                    long fourth = target - sum;
+                    if (hashset.contains(fourth)) {
                         List<Integer> temp = new ArrayList<>();
                         temp.add(nums[i]);
                         temp.add(nums[j]);
                         temp.add(nums[k]);
                         temp.add((int) fourth);
-                        Collections.sort(temp);
-                        mainSet.add(temp);
+                        temp.sort(Integer::compareTo);
+                        st.add(temp);
                     }
-                    hashSet.add(nums[k]);
+                    // put the kth element into the hashset:
+                    hashset.add((long) nums[k]);
                 }
             }
         }
-        List<List<Integer>> ans = new ArrayList<>(mainSet);
+        List<List<Integer>> ans = new ArrayList<>(st);
         return ans;
     }
 
@@ -77,38 +82,44 @@ public class Four_Sum {
     // So the total time complexity will be O(N^3).
     // Space Complexity: O(no. of quadruplets), This space is only used to store the answer. We are not using any extra
     // space to solve this problem. So, from that perspective, space complexity can be written as O(1).
-    static List<List<Integer>> fourSum(int[] nums, int target) {
-        // To return the answer
+    static List<List<Integer>> fourSum3(int[] nums, int target) {
+        int n = nums.length; // size of the array
         List<List<Integer>> ans = new ArrayList<>();
-        int n = nums.length;
 
+        // sort the given array:
         Arrays.sort(nums);
-        for (int i=0; i<n; i++) {
-            if (i > 0 && nums[i] == nums[i-1]) continue;
-            for (int j=i+1; j<n; j++) {
-                if (j > i+1 && nums[j] == nums[j-1]) continue;
-                int k=j+1;
-                int l=n-1;
+
+        // calculating the quadruplets:
+        for (int i = 0; i < n; i++) {
+            // avoid the duplicates while moving i:
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            for (int j = i + 1; j < n; j++) {
+                // avoid the duplicates while moving j:
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                // 2 pointers:
+                int k = j + 1;
+                int l = n - 1;
                 while (k < l) {
-                    int sum = nums[i] + nums[j] + nums[k] + nums[l];
-                    if (sum > target) {
-                        l--;
-                    }
-                    else if (sum < target) {
-                        k++;
-                    }
-                    else {
-                        List<Integer> list = new ArrayList<>();
-                        list.add(nums[i]);
-                        list.add(nums[j]);
-                        list.add(nums[k]);
-                        list.add(nums[l]);
-                        ans.add(list);
+                    long sum = nums[i];
+                    sum += nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+                    if (sum == target) {
+                        List<Integer> temp = new ArrayList<>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[k]);
+                        temp.add(nums[l]);
+                        ans.add(temp);
                         k++;
                         l--;
-                        while (k < l && nums[k] == nums[k-1]) k++;
-                        while (k < l && nums[l] == nums[l+1]) l--;
-                    }
+
+                        // skip the duplicates:
+                        while (k < l && nums[k] == nums[k - 1]) k++;
+                        while (k < l && nums[l] == nums[l + 1]) l--;
+                    } else if (sum < target) k++;
+                    else l--;
                 }
             }
         }
@@ -121,7 +132,7 @@ public class Four_Sum {
         int target = 0;
         // int[] arr = {1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5};
         // int target = 8;
-        System.out.println(fourSum(arr, target));
+        System.out.println(fourSum3(arr, target));
     }
 }
 
