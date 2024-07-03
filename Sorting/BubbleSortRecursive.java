180feb2edb6e1f02b9e2dc217e8d9d027b7faf10
NextGenGk <atkola12345@gmail.com>
2024-07-03 22:32:22 +0530

    Update Sorting - BubbleSortRecursive

--- Sorting/BubbleSortRecursive.java
+++ Sorting/BubbleSortRecursive.java
@@ -9,6 +9,7 @@ public class BubbleSortRecursive {
         if(n == 1) {
             return;
         }
+
         for(int i=0; i<n-1; i++) {
             if(arr[i] > arr[i+1]) {
                 // Swap
