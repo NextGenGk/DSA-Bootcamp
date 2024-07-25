f966d7f2a523552e84fa0a80a0c63aec3f3b76c7
NextGenGk <atkola12345@gmail.com>
2024-07-25 21:01:08 +0530

    Update LinkedList - SingleLinkedList (Code)

--- LinkedList/SingleLinkedList.java
+++ LinkedList/SingleLinkedList.java
@@ -5,6 +5,9 @@ public class SingleLinkedList {
     // Head Pointer
     Node head;
 
+    // Size of Linked List
+    int size;
+
     // Node Class
     static class Node {
         int data;      // the data value
@@ -23,62 +26,96 @@ public class SingleLinkedList {
 
     // Insertion (Insert a value at a specified location)
     void insert(int data, int location) {
+        if(location > size || location < 0) {
+            System.out.println("Invalid Index value");
+            return;
+        }
+
         Node node = new Node(data);
 
         if (location == 0) {
             node.next = head;
             head = node;
-            return;
+        } else {
+            Node prev = head;
+            for (int i = 0; i < location - 1; i++) {
+                prev = prev.next;
+            }
+            node.next = prev.next;
+            prev.next = node;
         }
-
-        Node prev = head;
-        for (int i=0; i<location-1; i++) {
-            prev = prev.next;
-        }
-        node.next = prev.next;
-        prev.next = node;
+        size++;
     }
 
     // Traversal
     public void printList() {
         if (head == null) {
             System.out.println("List is Empty");
+            return;
         }
 
         Node currNode = head;
         while (currNode != null) {
-            System.out.print(currNode.data+" -> ");
+            System.out.print(currNode.data + " -> ");
             currNode = currNode.next;
         }
         System.out.println("null");
+        System.out.println("Size: " + size);
+    }
+
+    // Length of a Linked List
+    int lengthOfLinkedList() {
+        if (head == null) {
+            System.out.println("List is Empty");
+            return 0;
+        }
+
+        Node currNode = head;
+        int count = 0;
+        while (currNode != null) {
+            currNode = currNode.next;
+            count++;
+        }
+        return count;
     }
 
     // Deletion (Delete a value at a specified location)
     void deletion(int location) {
-        if (location == 0) {
-            head = head.next;
+        if (location < 0 || location >= size) {
+            System.out.println("Invalid Index value");
             return;
         }
 
-        Node prev = head;
-        for (int i=0; i<location-1; i++) {
-            prev = prev.next;
+        if (location == 0) {
+            head = head.next;
+        } else {
+            Node prev = head;
+            for (int i = 0; i < location - 1; i++) {
+                prev = prev.next;
+            }
+            if (prev.next != null) {
+                prev.next = prev.next.next;
+            }
         }
-        prev.next = prev.next.next;
+        size--;
+    }
+
+    // Searching (Searching for a value)
+    int search(int value) {
+        Node currNode = head;
+        int index = 0;
+        while (currNode != null) {
+            if (currNode.data == value) {
+                return index;
+            }
+            currNode = currNode.next;
+            index++;
+        }
+        return -1; // Value not found
     }
 
     // Main Function
     public static void main(String[] args) {
-        // Creating a new Node with the value from the array
-//        Node node1 = new Node(10);
-//        Node node2 = new Node(20);
-//        Node node3 = new Node(30);
-//
-//        Node head = node1;
-//        head.next = node2;
-//        node2.next = node3;
-//        node3.next = null;
-
         // Creating a object of Linked List Class
         SingleLinkedList list = new SingleLinkedList();
         list.insert(80, 0);
@@ -86,7 +123,11 @@ public class SingleLinkedList {
         list.insert(100, 2);
         list.insert(70, 3);
         list.printList();
-        list.deletion(1);
-        list.printList();
+        list.deletion(2);
+        int res = list.search(90);
+        System.out.println(res);
+        System.out.println("Length of Linked List: " + list.lengthOfLinkedList());
     }
 }
+
+// Anuj Bhaiya (Video Explanation) : https://www.youtube.com/watch?v=4tU7d0TTiZQ&t
