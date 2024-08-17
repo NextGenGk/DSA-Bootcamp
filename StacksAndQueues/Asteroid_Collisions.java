a1f7c8e98198e8c06faa45f4e6e97085cbf941d2
GAURAV KUMAR <atkola12345@gmail.com>
2024-08-17 16:01:41 +0530

    Update Stack & Queue - Asteroid_Collisions (Code)

--- StacksAndQueues/Asteroid_Collisions.java
+++ StacksAndQueues/Asteroid_Collisions.java
@@ -60,8 +60,29 @@ public class Asteroid_Collisions {
 
     // Main Function
     public static void main(String[] args) {
-        int[] arr1 = {4, 7, 1, 1, 2, -3, -7, 17, 15, -16};
-        int[] result1 = asteroidCollisions(arr1);
-        System.out.println("Result 1: " + Arrays.toString(result1));  // Expected output: [4, 17]
+        int[] arr = {4, 7, 1, 1, 2, -3, -7, 17, 15, -16};
+        int[] result1 = asteroidCollisions(arr);
+        System.out.println("Result 1: " + Arrays.toString(result1));
     }
 }
+
+// Output : [4, 17]
+
+// Approach : Optimal Solution
+/*
+Intuition & Idea Behind Using List :
+
+1. Positive asteroids move right and are added to the list because they don't affect each other.
+2. Negative asteroids move left and may collide with the last right-moving asteroid in the list.
+    i. If the negative asteroid is stronger, it destroys the last positive asteroid.
+   ii. If both are the same size, both are destroyed.
+  iii. If the positive asteroid is stronger, the negative one is destroyed.
+
+The List acts like a flexible stack, allowing us to easily manage asteroid collisions by adding
+and removing elements from the end. It helps handle dynamic changes in asteroid counts without
+worrying about fixed sizes.
+
+At the end, whatever is left in the list are the surviving asteroids.
+ */
+
+// Striver's (Video Explanation) : https://www.youtube.com/watch?v=_eYGqw_VDR4
