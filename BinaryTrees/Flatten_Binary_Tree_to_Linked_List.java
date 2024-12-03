32a25f8d4d6af2f978ef828844fc7075f7840d7d
GAURAV KUMAR <atkola12345@gmail.com>
2024-12-04 00:11:46 +0530

    Add Binary Trees - Flatten_Binary_Tree_to_Linked_List (Better and Optimal Code)

--- BinaryTrees/Flatten_Binary_Tree_to_Linked_List.java
+++ BinaryTrees/Flatten_Binary_Tree_to_Linked_List.java
@@ -1,5 +1,7 @@
 package BinaryTrees;
 
+import java.util.Stack;
+
 public class Flatten_Binary_Tree_to_Linked_List {
 
     // Definition for a binary tree node.
@@ -29,7 +31,18 @@ public class Flatten_Binary_Tree_to_Linked_List {
 
     // Method 1 : Brute Force
 
-    class Solution {
+    // Time Complexity: O(N) where N is the number of nodes in the Binary Tree.
+    // Each node of the binary node is visited exactly once. At each node,
+    // constant-time operations are performed. Hence the time complexity is O(N).
+
+    // Space Complexity : O(log2N)where N is the number of nodes in the Binary Tree.
+    // There are no additional data structures or space used but the auxiliary stack
+    // space is used during recursion. Since the recursion depth can be at most equal
+    // to the height to the Binary Tree, the space complexity is O(H) where H is the
+    // height of the Binary Tree. In the ideal case, H = log2N and in the worst case
+    // H = N (skewed tree).
+
+    static class Solution_Brute {
         // Initialize a global variable
         // 'prev' to keep track of the
         // previously processed node.
@@ -71,6 +84,128 @@ public class Flatten_Binary_Tree_to_Linked_List {
         }
     }
 
+    // Method 2 : Better Solution
+
+    // Time Complexity: O(N) where N is the number of nodes in the Binary Tree.
+    // Each node of the binary node is visited exactly once. At each node, constant-
+    // time operations are performed. Hence the time complexity is O(N).
+
+    // Space Complexity : O(log2N) where N is the number of nodes in the Binary Tree.
+    // There are no additional data structures or space used but the auxiliary stack
+    // space is used during recursion. Since the recursion depth can be at most equal
+    // to the height to the Binary Tree, the space complexity is O(H) where H is the
+    // height of the Binary Tree. In the ideal case, H = log2N and in the worst case
+    // H = N (skewed tree).
+
+    static class Solution_Better {
+        // Initialize a global variable
+        // 'prev' to keep track of the
+        // previously processed node.
+        TreeNode prev = null;
+
+        // Function to flatten a binary tree
+        // to a right next Linked List structure
+        public static void flatten(TreeNode root) {
+            // Base case: If the current
+            // node is null, return.
+            if (root == null) {
+                return;
+            }
+            // Use a stack for
+            // iterative traversal.
+            Stack<TreeNode> st = new Stack<>();
+            // Push the root node
+            // onto the stack.
+            st.push(root);
+
+            // Continue the loop until
+            // the stack is empty.
+            while (!st.empty()) {
+                // Get the top node from the stack.
+                TreeNode cur = st.pop();
+
+                if (cur.right != null) {
+                    // Push the right child
+                    // onto the stack.
+                    st.push(cur.right);
+                }
+
+                if (cur.left != null) {
+                    // Push the left child
+                    // onto the stack.
+                    st.push(cur.left);
+                }
+
+                if (!st.empty()) {
+                    // Connect the right child to
+                    // the next node in the stack.
+                    cur.right = st.peek();
+                }
+
+                // Set the left child to null to
+                // form a right-oriented linked list.
+                cur.left = null;
+            }
+        }
+    }
+
+    // Method 3 : Optimal Solution
+
+    // Time Complexity: O(2N) where N is the number of nodes in the Binary Tree.
+    // The time complexity is linear, as each node is visited at most twice
+    // (once for establishing the temporary link and once for reverting it).
+    // In each step, we perform constant-time operations, such as moving to the
+    // left or right child and updating pointers.
+
+    // Space Complexity: O(1) The space complexity is constant, as the algorithm
+    // uses only a constant amount of extra space irrespective of the input size.
+    // Morris Traversal does not use any additional data structures like stacks or
+    // recursion, making it an in-place algorithm.
+    // The only space utilised is for a few auxiliary variables, such as pointers to
+    // current and in-order predecessor nodes.
+
+    static class Solution_Optimal {
+        // Function to flatten a binary tree
+        // to a right next Linked List structure
+        public void flatten(TreeNode root) {
+            // Initialize a pointer
+            // 'curr' to the root of the tree
+            TreeNode curr = root;
+
+            // Iterate until 'curr'
+            // becomes NULL
+            while (curr != null) {
+                // Check if the current
+                // node has a left child
+                if (curr.left != null) {
+                    // If yes, find the rightmost
+                    // node in the left subtree
+                    TreeNode pre = curr.left;
+                    while (pre.right != null) {
+                        pre = pre.right;
+                    }
+
+                    // Connect the rightmost node in
+                    // the left subtree to the current
+                    // node's right child
+                    pre.right = curr.right;
+
+                    // Move the entire left subtree to the
+                    // right child of the current node
+                    curr.right = curr.left;
+
+                    // Set the left child of
+                    // the current node to NULL
+                    curr.left = null;
+                }
+
+                // Move to the next node
+                // on the right side
+                curr = curr.right;
+            }
+        }
+    }
+
     // Print the preorder traversal of the
     // Original Binary Tree
     public static void printPreorder(TreeNode root) {
@@ -100,14 +235,17 @@ public class Flatten_Binary_Tree_to_Linked_List {
         root.left.left = new TreeNode(4);
         root.left.right = new TreeNode(5);
         root.left.right.right = new TreeNode(6);
-        root.right.right = new TreeNode(7);
         root.right.left = new TreeNode(8);
+        root.right.right = new TreeNode(7);
 
         System.out.print("Binary Tree Preorder: ");
         printPreorder(root);
         System.out.println();
 
-        Solution.flatten(root);
+        // Uncomment the desired solution to test
+        // Solution_Brute.flatten(root);
+        // Solution_Better.flatten(root);
+        new Solution_Optimal().flatten(root);
 
         System.out.print("Binary Tree After Flatten: ");
         printFlattenTree(root);
@@ -115,7 +253,9 @@ public class Flatten_Binary_Tree_to_Linked_List {
     }
 }
 
-// Output : Binary Tree Preorder: 1 2 4 5 6 3 8 7, Binary Tree After Flatten: 1 2 4 5 6 3 8 7
+// Output :
+// Binary Tree Preorder: 1 2 4 5 6 3 8 7,
+// Binary Tree After Flatten: 1 2 4 5 6 3 8 7
 
 // Algorithm / Intuition : Brute Force
 /*
@@ -159,6 +299,56 @@ Set the left child of the current node to null since we are flattening the binar
 tree to a linked list and there should be no left child.
 
 Step 6:Update `prev` to the current node for the next iteration and recursion step.
+ */
+
+// Algorithm / Intuition : Better Solution
+/*
+Algorithm / Intuition
+This approach is an extension of the previous approach where we use a stack to simulate recursion.
+
+Algorithm
+
+Step 1: Base Case: If the root node is null, we return as there is no tree to flatten.
+
+Step 2: Initialise Stack: Create a stack to perform an iterative traversal of the binary tree.
+Push the root node onto the sack to start the traversal.
+
+Step 3: Iterative Traversal with Stack: While the stack is not empty, repeat the following steps:
+
+1. Pop the top node from the stack.
+2. If the popped node has a right child, push it onto the stack. This ensures that the right child
+   is processed after the left child nodes.
+3. If the popped node has a left child, push it onto the stack.
+4. If the stack is not empty after pushing the left child, connect the right pointer of the
+current node (popped from the stack) to the top node of the stack. This creates the linked
+list structure by setting the next pointer.
+5. Set the left pointer of the current node to null as we have to form a right skewed linked list.
+
+Step 4: Once the stack becomes empty, the traversal is complete and the binary tree is
+flattened into a right next linked list structure.
+ */
+
+// Algorithm / Intuition : Optimal Solution
+/*
+Morris Traversal is an algorithm that allows preorder/inorder tree traversal without
+using any extra space for stack or recursion. It utilises threaded binary trees to
+traverse the tree without requiring a stack for saving the path. Read more about Morris
+Preorder Traversal here
+
+Algorithm
+Step 1: Start with the current node as the root of the tree.
+
+Step 2: While the current node is not null we traverse the tree in a while loop:
+
+1. If the current node has a left child:
+    1. Find the rightmost node in the left subtree.
+    2. Connect the rightmost node of the left subtree to the current node’s right child.
+    3. Update the current node’s right child to be its left child.
+    4. Set the current node’s left child to null.
+2. Move to the current node’s right child.
+3. If the current node has no right child:
+    1. Move to the current node’s left child (if it exists) or null.
+Step 3: Repeat until all nodes are processed.
  */
 
 // Striver (Video Explanation) : https://www.youtube.com/watch?v=sWf7k1x9XR4
