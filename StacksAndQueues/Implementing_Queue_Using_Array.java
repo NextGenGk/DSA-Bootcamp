ab8eb58ee7e155afdbde099fc08289210521a87d
GAURAV KUMAR <atkola12345@gmail.com>
2025-03-21 19:05:44 +0530

    Update Stack and Queues - Implementing_Queue_Using_Array

--- StacksAndQueues/Implementing_Queue_Using_Array.java
+++ StacksAndQueues/Implementing_Queue_Using_Array.java
@@ -1,119 +1,6 @@
 package StacksAndQueues;
 
 class Implementing_Queue_Using_Array {
-    int[] arr;
-    int front;
-    int rear;
-    int capacity;
-    int currentSize; // Variable to keep track of the current size of the queue
-
-    // Constructor
-    public Implementing_Queue_Using_Array(int capacity) {
-        this.arr = new int[capacity];
-        this.front = -1;
-        this.rear = -1;
-        this.capacity = capacity;
-        this.currentSize = 0; // Initialize size to 0
-        System.out.println("The Queue is created with the size of " + capacity);
-    }
-
-    // isEmpty()
-    public boolean isEmpty() {
-        if (currentSize == 0) {
-            System.out.println("Queue is Empty");
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    // isFull()
-    public boolean isFull() {
-        if (currentSize == capacity) {
-            System.out.println("Queue is Full");
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    // enQueue()
-    public void enQueue(int value) {
-        if (isFull()) {
-            System.out.println("Queue is full");
-        } else {
-            if (isEmpty()) {
-                front = 0;
-            }
-            rear++;
-            arr[rear] = value;
-            currentSize++; // Increment size when a new element is enqueued
-            System.out.println("Successfully inserted " + value + " into the queue");
-        }
-    }
-
-    // deQueue()
-    public int deQueue() {
-        if (isEmpty()) {
-            System.out.println("The Queue is empty");
-            return -1;
-        } else {
-            int result = arr[front];
-            front++;
-            currentSize--; // Decrement size when an element is dequeued
-            // Reset front and rear if the queue is now empty
-            if (currentSize == 0) {
-                front = -1;
-                rear = -1;
-            }
-            return result;
-        }
-    }
-
-    // peek()
-    public int peek() {
-        if (isEmpty()) {
-            System.out.println("The Queue is empty");
-            return -1;
-        } else {
-            return arr[front];
-        }
-    }
-
-    // size() method to return the current size of the queue
-    public int size() {
-        return currentSize;
-    }
-
-    // deleteQueue()
-    public void deleteQueue() {
-        arr = null;
-        front = -1;
-        rear = -1;
-        currentSize = 0; // Reset size to 0 when the queue is deleted
-        System.out.println("Deleted Queue Successfully");
-    }
-
-    // Main Function
-    public static void main(String[] args) {
-        Implementing_Queue_Using_Array queueArray = new Implementing_Queue_Using_Array(4);
-        queueArray.enQueue(10);
-        queueArray.enQueue(20);
-        queueArray.enQueue(30);
-        queueArray.enQueue(40);
-
-        System.out.println("Current queue size: " + queueArray.size());
-
-        int result = queueArray.peek();
-        System.out.println("Front element: " + result);
-
-        result = queueArray.deQueue();
-        System.out.println("Dequeued element: " + result);
-
-        System.out.println("Current queue size after dequeue: " + queueArray.size());
-
-        queueArray.deleteQueue();
-    }
 
     // Implementation v2
     static class Queue {
@@ -208,7 +95,6 @@ class Implementing_Queue_Using_Array {
             System.out.println("Front element: " + q.front());
         }
     }
-
 }
 
 // Output:
