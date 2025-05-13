1bc8ed91ec39010bd682467acf2411fe8b360187
GAURAV KUMAR <atkola12345@gmail.com>
2025-05-13 21:38:46 +0530

    Update Arrays - Four_Sum.java (code)

--- Arrays/Four_Sum.java
+++ Arrays/Four_Sum.java
@@ -130,15 +130,15 @@ public class Four_Sum {
     public static void main(String[] args) {
         int[] arr = {1,0,-1,0,-2,2};
         int target = 0;
-        // int[] arr = {1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5};
+        // int[] arr1 = {1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5};
         // int target = 8;
         System.out.println(fourSum3(arr, target));
     }
 }
 
-// Output
-// [[-2, -1, 1, 2], [-2, 0, 0, 2], [-1, 0, 0, 1]]
-// [[1, 1, 1, 5], [1, 1, 2, 4], [1, 2, 2, 3], [1, 1, 3, 3]]
+// Output : 
+// [[-2, -1, 1, 2], [-2, 0, 0, 2], [-1, 0, 0, 1]] (Output arr)
+// [[1, 1, 1, 5], [1, 1, 2, 4], [1, 2, 2, 3], [1, 1, 3, 3]] (Output arr1)
 
 // Algorithm : Brute Force (Using 4 loops)
 /*
