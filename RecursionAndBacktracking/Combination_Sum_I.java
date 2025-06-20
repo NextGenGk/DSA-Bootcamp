7cb3bed4b58f1b31dd9fb9992b8fc26506760257
GAURAV KUMAR <atkola12345@gmail.com>
2025-06-20 23:06:56 +0530

    Update Recursion and Backtracking - Combination_Sum_I.java

--- RecursionAndBacktracking/Combination_Sum_I.java
+++ RecursionAndBacktracking/Combination_Sum_I.java
@@ -14,7 +14,7 @@ public class Combination_Sum_I {
     // Why not (2^n) but (2^t) (where n is the size of an array)?
     // Assume that there is 1 and the target you want to reach is 10 so 10 times you can
     // “pick or not pick” an element.
-    // Space Complexity: O(k*x), k is the average length and x is the no. of combinations
+    // Space Complexity: O(k*x), k is the average length of list ds and x is the no. of combinations
     
     // Main function to find all unique combinations that sum up to the target
     public static List<List<Integer>> combinationSum(int[] candidates, int target) {
