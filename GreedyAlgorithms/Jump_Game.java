2cbf387b7e72acbb4f0e5910f1725cedec4def75
GAURAV KUMAR <atkola12345@gmail.com>
2025-07-01 23:27:32 +0530

    Update Greedy Algorithms - Jump_Game.java (Code)

--- GreedyAlgorithms/Jump_Game.java
+++ GreedyAlgorithms/Jump_Game.java
@@ -8,6 +8,7 @@ public class Jump_Game {
     // Space Complexity: O(1) as the algorithm uses only a constant amount of extra space regardless
     // of the size of the input array. It does not require any additional data structures that scale
     // with the input size.
+    
     // Function to determine if you can reach the last index
     public static boolean canJump(int[] nums) {
         // Initialize the maximum index that can be reached
