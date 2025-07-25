81fd16753962871f40cb4a26c9ce61501114a7fa
GAURAV KUMAR <atkola12345@gmail.com>
2025-07-25 23:10:25 +0530

    Update Arrays - Find_Largest_Element.java (Code)

--- Arrays/Find_Largest_Element.java
+++ Arrays/Find_Largest_Element.java
@@ -8,10 +8,14 @@ public class Find_Largest_Element {
     public static int findLargest(int[] arr) {
 
         // Method 1 : Brute Force
+        // Time Complexity: O(N*log(N))
+        // Space Complexity: O(N)
 //        Arrays.sort(arr);
 //        return arr[arr.length-1];
 
         // Method 2 : Optimal Solution
+        // Time Complexity: O(N)
+        // Space Complexity: O(1)
         int max = arr[0];
         for(int i=1; i< arr.length; i++) {
             if (arr[i] > max) {
@@ -36,9 +40,6 @@ public class Find_Largest_Element {
 1. Sort the array in ascending order.
 2. Print the (size of the array -1)th index.
 3. For Sorting Method:
-
-Time Complexity: O(N*log(N))
-Space Complexity: O(N)
 */
 
 // Approach 2 : Using a max Variable
@@ -47,9 +48,6 @@ Space Complexity: O(N)
 2. Use a for loop and compare it with other elements of the array
 3. If any element is greater than the max value, update max value with the element’s value
 4. Print the max variable.
-
-Time Complexity: O(N)
-Space Complexity: O(1)
  */
 
 // Striver (Videos Explanation) - https://www.youtube.com/watch?v=37E9ckMDdTk
