4e93665129e749a10438fd41b03c81f95a1f6ddc
GAURAV KUMAR <atkola12345@gmail.com>
2025-08-01 22:53:34 +0530

    Update Arrays - Longest_Consecutive_Sequence.java (add new solution / method)

--- Arrays/Longest_Consecutive_Sequence.java
+++ Arrays/Longest_Consecutive_Sequence.java
@@ -83,6 +83,29 @@ public class Longest_Consecutive_Sequence {
         return longest;
     }
 
+    // Optimal Solution v2 
+    static int longestConsecutive(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int longest = 0;
+        for (int num : set) {
+            if (!set.contains(num - 1)) {  // Start of a sequence
+                int currentNum = num;
+                int count = 1;
+
+                while (set.contains(currentNum + 1)) {
+                    currentNum++;
+                    count++;
+                }
+                longest = Math.max(longest, count);
+            }
+        }
+        return longest;
+    }
+
     // Main Function
     public static void main(String[] args) {
         int[] arr = {100, 200, 1, 3, 2, 4};
