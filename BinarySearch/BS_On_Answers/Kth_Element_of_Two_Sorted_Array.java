f9d5504cbf5645e7632beed05f17b7ad854b5d4f
AI PC <atkola12345@gmail.com>
2025-09-05 23:04:48 +0530

    Update Arrays - Kth_Element_of_Two_Sorted_Array (update variables name)

--- BinarySearch/BS_On_Answers/Kth_Element_of_Two_Sorted_Array.java
+++ BinarySearch/BS_On_Answers/Kth_Element_of_Two_Sorted_Array.java
@@ -108,8 +108,8 @@ public class Kth_Element_of_Two_Sorted_Array {
             // calculate l1, l2, r1, and r2
             int l1 = (mid1 > 0) ? a[mid1 - 1] : Integer.MIN_VALUE;
             int l2 = (mid2 > 0) ? b[mid2 - 1] : Integer.MIN_VALUE;
-            int r1 = (mid1 < n) ? a[mid1] : Integer.MAX_VALUE;
-            int r2 = (mid2 < m) ? b[mid2] : Integer.MAX_VALUE;
+            int r1 = (mid1 < m) ? a[mid1] : Integer.MAX_VALUE;
+            int r2 = (mid2 < n) ? b[mid2] : Integer.MAX_VALUE;
 
             if (l1 <= r2 && l2 <= r1) {
                 return Math.max(l1, l2);
