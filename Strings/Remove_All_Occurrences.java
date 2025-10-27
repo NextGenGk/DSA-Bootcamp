16be0aba4fdabdb07f8ff2f3aea2a24eb33ec58c
GAURAV KUMAR <atkola12345@gmail.com>
2025-10-27 22:49:22 +0530

    Update Stings - Remove_All_Occurrences (Update comment for removeOccurrences method)
    Clarified comment for optimal approach method.

--- Strings/Remove_All_Occurrences.java
+++ Strings/Remove_All_Occurrences.java
@@ -15,7 +15,7 @@ public class Remove_All_Occurrences {
         System.out.println(sol.removeOccurrences(s2, part2)); // Output: "ab"
     }
 
-    // Method 1: Optimal Approach
+    // Method 1: Optimal Approach (Using String's inbuilt methods)
     // Time Complexity: O(n * m) in the worst case, where n is the length of s and m is the length of part.
     // Space Complexity: O(1) if we ignore the space used for the output string.
     public String removeOccurrences(String s, String part) {
