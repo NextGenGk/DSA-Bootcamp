352f8f812d1d1e0639c1c3d7d19937ad86dbfe76
GAURAV KUMAR <atkola12345@gmail.com>
2026-06-16 23:51:11 +0530

    Fix comment formatting in Subset_Sum_I.java

--- RecursionAndBacktracking/Subset_Sum_I.java
+++ RecursionAndBacktracking/Subset_Sum_I.java
@@ -6,7 +6,7 @@ import java.util.Collections;
 public class Subset_Sum_I {
 
     // Method 1 : Optimal Solution
-    // Time Complexity: O(2^n)+O(2^n log(2^n)). Each index has two ways. You can either pick
+    // Time Complexity: O(2^n) + O(2^n log(2^n)). Each index has two ways. You can either pick
     // it up or not pick it. So for n index time complexity for O(2^n) and for sorting it will
     // take (2^n log(2^n)).
     // Space Complexity: O(2^n) for storing subset sums, since 2^n subsets can be generated for
