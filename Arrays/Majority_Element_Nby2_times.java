d179d66dce84cf3ef9c53a6c750997eb5c6608ee
GAURAV KUMAR <atkola12345@gmail.com>
2026-06-19 23:42:37 +0530

    Update Majority_Element_Nby2_times.java

--- Arrays/Majority_Element_Nby2_times.java
+++ Arrays/Majority_Element_Nby2_times.java
@@ -158,4 +158,4 @@ public class Majority_Element_Nby2_times {
 9. If count2 > n/2, then return result.
  */
 
-// Striver (Video Explanation) - https://www.youtube.com/watch?v=nP_ns3uSh80
+// Striver's (Video Explanation) - https://www.youtube.com/watch?v=nP_ns3uSh80
